00e657ec181e603fc9c73f36e0054ba5ed3bd2ef
Vinod Lala <vinod_lala@usa.net>
2013-10-07 08:10:14 -0400

    Monday

--- worldwalker.rb
+++ worldwalker.rb
@@ -10,7 +10,8 @@ require 'sinatra'
 require 'sinatra/reloader' if development?
 require 'json'
 
-BASE_URL = "http://stackworld.herokuapp.com"
+#BASE_URL = "http://stackworld.herokuapp.com"
+BASE_URL = "http://stormy-everglades-2242.herokuapp.com"
 WORLD_START_URL = "#{BASE_URL}/room/1"
 
 require_relative 'environments'
@@ -36,50 +37,30 @@ $url_to_page = {}
 $visited_links = []
 
 def crawl_link(link, depth = 0)
-
   # 1. Get the page using open
   page_html = open(link)
-
   # 2. Add the page's URL to the visited links array
   if !$visited_links.include?(link)
     $visited_links << link
     puts link
   end
-
   # 3. Use Nokogiri to parse the page into a Nokogiri document
   page_doc = Nokogiri::HTML(page_html)
-
   # 4. Get any page information like room name
-  room_name = page_doc.css('h1')[0].text
-
+  room_name = page_doc.css('h1')[0].text.strip
   # 5. Get outbound links that we care about (room connections)
   links = page_doc.css('a')
   filtered_links = links.select do |link|
     link['href'] =~ /room\/\d+$/
   end
 
-
-
-
   filtered_links.each do |link|
     if !$visited_links.include?("#{BASE_URL}" + link['href'])
-  # # 6. Save the page information as a new Page object, put it into a
-  # # hash called something like $url_to_page which maps URLS to the
-  # # Page objects.
-  # # $url_to_page << link['href']
+      # 6. Save the page info into a hash called $url_to_page which maps URLS to Page objects.
       linkstring = "#{BASE_URL}" + link['href']
       $url_to_page[linkstring] = Page.new({:url => linkstring, :title => room_name})
-#   $url_to_page << Page.new(:url => link['href'], :title => room_name)
 
-  #   :links => links
-  #   # :pages =
-  #   # :index =
-
-  # 7. For each outbound link, call get_and_crawl_link(link)
-  # This method will keep calling itself until it's visited every
-  # room on the site
-      #linkstring = "#{BASE_URL}" + link['href']
-      #puts linkstring
+      # 7. For each outbound link, call get_and_crawl_link(link)
       crawl_link("#{BASE_URL}" + link['href'])
     end
   end
@@ -87,6 +68,13 @@ def crawl_link(link, depth = 0)
 end
 
 crawl_link(WORLD_START_URL)
+
+puts "$visited_links"
+puts $visited_links
+
+puts "$url_to_page"
+puts $url_to_page.to_s
+
 
 ## Start Sinatra App here:
 
